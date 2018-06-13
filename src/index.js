303a313cb1ccf512e327c0ad5ec98f19349c26db
Jim Kelly <jameskelly@synovus.com>
2018-06-13 11:15:54 -0400

    Adding Images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Logo from './logo.svg';
 
 function component() {
   var element = document.createElement('h1');
@@ -7,6 +8,13 @@ function component() {
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack!'], ' ');
   element.classList.add('hello');
+
+  // Add the logo
+  var myLogo = new Image(200);
+  myLogo.classList.add('logo');
+  myLogo.src = Logo;
+
+  element.appendChild(myLogo);
 
   return element;
 }
